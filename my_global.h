742cbf52023d9a8b2e95db68e9462d99bc17810f
Raymond Milad <raymond.milad92@gmail.com>
2013-11-18 18:59:15 +0200

    Update my_global.h

--- my_global.h
+++ my_global.h
@@ -5,10 +5,11 @@
  *      Author: raymond
  */
 
-
-#define  MAX_DATA_SIZE  10
+#define  MAX_PACKET_SIZE  508
+#define  MAX_DATA_SIZE  500
 #define  ACK_PACKET_SIZE 8
-
+static int SENT_ACKETD = 1, SENT_NOT_ACKED = 2, NOT_SENT_AVAIL = 3, NOT_AVAIL =
+		4;
 
 /* Data-only packets */
 struct packet {
@@ -21,7 +22,6 @@ struct packet {
 	/* Not always 500 bytes, can be less */
 };
 
-#define  MAX_PACKET_SIZE  MAX_DATA_SIZE + sizeof(packet)
 /* Ack-only packets are only 8 bytes */
 struct ack_packet {
 	uint16_t cksum; /* Optional bonus part */
