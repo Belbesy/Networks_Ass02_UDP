2321a49f247162fe395da1560175a883aa7abb68
Raymond Milad <raymond.milad92@gmail.com>
2013-11-18 18:58:36 +0200

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -15,7 +15,7 @@
 #include <fstream>
 #include <time.h>
 #include <stdlib.h>     /* srand, rand */
-
+#include <pthread.h>
 using namespace std;
 
 unsigned short servPort; //server,port
@@ -27,6 +27,15 @@ struct sockaddr_in clientAddr;
 int recv_msg_size;
 char recv_msg[MAX_PACKET_SIZE];
 char* filename;
+
+//----------------------------------------------
+//Selective Repeat
+bool is_sr;
+int initial_swindow, window_base, available_cells, window_size, next_cell;
+int *window;
+pthread_t *threads;
+//Selective Repeat
+//----------------------------------------------
 
 void error(string str) {
 	cout << str << endl;
@@ -59,6 +68,20 @@ void initialize_server() {
 
 	//initialize random number generator
 	srand(seed);
+	if (is_sr) {
+		initial_swindow = 1;//TODO
+		window = (int*) (malloc(max_swnd * sizeof(int)));
+		threads = (pthread_t*) (malloc(max_swnd * sizeof(pthread_t)));
+		available_cells = initial_swindow;
+		window_size = initial_swindow;
+		window_base = 0;
+		next_cell = 0;
+		for (int i = 0; i < max_swnd; i++)
+			if (i < initial_swindow)
+				window[i] = NOT_SENT_AVAIL;
+			else
+				window[i] = NOT_AVAIL;
+	}
 }
 
 void receive_request() {
@@ -70,12 +93,13 @@ void receive_request() {
 		error("recvfrom() failed");
 	printf("Handling client %s\n", inet_ntoa(clientAddr.sin_addr));
 	cout << recv_msg_size << endl;
-//	cout << recv_msg << endl;
+	cout << recv_msg << endl;
 }
 
 bool packet_fall() {
 	int r = rand() % 11; //generate random number 0-10
-	double d = r / 10; //d 0..1
+	cout << "random " << r << endl;
+	double d = r / 10.0; //d 0..1
 	if (d < loss_prob)
 		return true;
 	return false;
@@ -85,50 +109,48 @@ void reply() {
 	//reply with the file
 	packet *request_packet = (packet*) recv_msg;
 	filename = request_packet->data;
-
-	cout << request_packet->len << "- "<< request_packet->seqno << " file name : ---------- (" ;
-
-	for(int i=0; i<request_packet->len; i++)
-		cout << i << ":" <<  request_packet->data[i] << endl;
-	cout << endl;
-
 	ifstream file(filename);
 	uint32_t last_seqno = request_packet->seqno - 1;
-	char write_buff[MAX_DATA_SIZE];
 
-	fd_set readfds;
-	FD_SET(sock, &readfds);
-	struct timeval tv;
-	tv.tv_sec = 1;
-	int n = sock + 1;
 	//divide file into segments and send them
 	while (!file.eof()) {
 		packet * p = new packet();
 		p->seqno = ++last_seqno;
-		file.read(write_buff, MAX_DATA_SIZE);
-		p->len = strlen(write_buff) + 1;
-
-		if(file.eof())
-			write_buff[p->len -1] = EOF;
-		
-		cout <<  " length : "<< p->len << endl;
+		file.read(p->data, MAX_DATA_SIZE);
+		p->len = strlen(p->data);
+		if (file.eof()) {
+			p->data[p->len] = EOF;
+			p->len = p->len + 1;
+		}
 		bool acked = false;
 		while (!acked) {
 			//Check if packet should fall (loss simulation)
 			if (!packet_fall()) {
+				cout << "Sending seq " << p->seqno << "   len " << p->len
+						<< endl;
+				//						<< "  " << p->data << endl;
 				//send the segment
-				if (sendto(sock, write_buff, p->len, 0,
-						(struct sockaddr *) &clientAddr, sizeof(clientAddr))
-						!= p->len)
-					error(
-							"sendto() sent a different number of bytes than expected");
+				int t;
+				if ((t = sendto(sock, p, sizeof(packet), 0,
+						(struct sockaddr *) &clientAddr, sizeof(clientAddr)))
+						!= sizeof(packet))
+					error("send different number of bytes");
+			} else {
+				cout << "PAcket fall " << p->seqno << endl;
 			}
 			//wait for ack or time out
+			fd_set readfds;
+			FD_ZERO(&readfds);
+			FD_SET(sock, &readfds);
+			struct timeval tv;
+			tv.tv_sec = 1;
+			tv.tv_usec = 500000;
+			int n = sock + 1;
 			int rv = select(n, &readfds, NULL, NULL, &tv);
 			if (rv == -1) {
 				error("select"); // error occurred in select()
 			} else if (rv == 0) {// time out
-				cout << "Time Out" << endl;
+				cout << "Time Out" << endl << "Retransmission" << endl;
 			} else {
 				unsigned int cliAddrLen = sizeof(clientAddr);
 				if ((recv_msg_size = recvfrom(sock, recv_msg, MAX_PACKET_SIZE,
@@ -137,21 +159,16 @@ void reply() {
 				ack_packet * ack = (ack_packet*) recv_msg;
 				if (ack->ackno == last_seqno)//check whether this is the waited ack.
 					acked = true;
+				cout << "Receive Ack" << ack->ackno << "  " << acked << endl;
 			}
 		}
 
-		//		clock_t start_time = clock();
-		//		clock_t curr_time = clock();
-		//		double diff = (start_time - curr_time) / (CLOCKS_PER_SEC / 1000);
-		//		bool sent = false;
-		//		while (diff < time_out && !sent) {
-		//
-		//		}
 	}
 	file.close();
 }
 
 int main(int argc, char *argv[]) {
+	is_sr = false;//not selective repeat for now
 	initialize_server();
 	receive_request();
 	reply();
